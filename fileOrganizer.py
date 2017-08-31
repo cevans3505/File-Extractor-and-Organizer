babc5d2ae01150e1f33da2b651b09aa3afea5869
cevans3505 <cevans3505@gmail.com>
2017-08-31 14:12:55 -0400

    Add files via upload
    ver. 0.2

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -4,7 +4,7 @@
 Created by Distilled
 Contact: distilled@protonmail.com
 31 August 2017
-Version 0.1
+Version 0.2
 """
 
 # Simple Python script to extract all files from
@@ -13,8 +13,26 @@ Version 0.1
 import shutil
 import os 
 
+# What file extensions do you want to preserve? (Wildcard character: *)
+# Example 1 (Extract specific file types):  keepExts = [".png", ".txt", ".mp*"]
+# Example 2 (Extract all file types):       keepExts = [".*"]
+keepExts = [".txt", ".zip"]
+
+# Define destination directory (absolute filepath)
+# Default (if blank): parent dir
+destDir = 'C:\\Users\cevan\\Desktop\\Test Py Dir\\destDir'
+
 # Get absolute dir path where script executed
 cwd = os.getcwd()
+
+# Create empty arrays to contain log info for output to console at end of script
+movedFiles = []
+deletedDirectories = []
+
+if destDir == "":
+    destDir = cwd
+else:
+    destDir = destDir
 
 # Generator that walks DOWN the folder tree
 walker = os.walk(cwd)
@@ -23,11 +41,37 @@ walker = os.walk(cwd)
 for data in walker:
     # Get files contained within sub dir(s)
     for files in data[2]:
-        try:
-            # Move file(s) to parent dir
-            shutil.move(data[0] + os.sep + files, cwd)
-        except shutil.Error:
-            continue
+        if os.path.splitext(files)[1] in keepExts:
+            try:
+                # Move file(s) to destDir
+                shutil.move(data[0] + os.sep + files, destDir)
+                movedFiles.append(files)
 
-exitFunc = input("Press 'Enter' key to exit.")
+            except shutil.Error:
+                continue
+
+print("Successfully moved the following file(s) into " + cwd + ":\n")
+for i in movedFiles:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#exitFunc = input("Press 'Enter' key to exit.")
 exit()
