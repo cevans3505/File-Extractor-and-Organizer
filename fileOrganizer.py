55e893976d5a539f582a3c4990c3f99a497343ea
cevans3505 <cevans3505@gmail.com>
2017-09-03 04:32:37 -0400

    v0.2.1

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -4,7 +4,7 @@
 Created by Distilled
 Contact: distilled@protonmail.com
 31 August 2017
-Version 0.2
+Version 0.2.1
 """
 
 # Simple Python script to extract all files from
@@ -16,11 +16,14 @@ import os
 # What file extensions do you want to preserve? (Wildcard character: *)
 # Example 1 (Extract specific file types):  keepExts = [".png", ".txt", ".mp*"]
 # Example 2 (Extract all file types):       keepExts = [".*"]
-keepExts = [".txt", ".zip"]
+keepExts = [""]
 
 # Define destination directory (absolute filepath)
 # Default (if blank): parent dir
-destDir = 'C:\\Users\cevan\\Desktop\\Test Py Dir\\destDir'
+destDir = ''
+
+# Which directories do you want to omit? (absolute filepath)
+skipDirs = ['']
 
 # Get absolute dir path where script executed
 cwd = os.getcwd()
@@ -34,23 +37,24 @@ if destDir == "":
 else:
     destDir = destDir
 
-# Generator that walks DOWN the folder tree
+# 1st walk
 walker = os.walk(cwd)
 
 # 'data' returns a 3 index tuple (dirpath, dirnames, filenames)
 for data in walker:
-    # Get files contained within sub dir(s)
-    for files in data[2]:
-        if os.path.splitext(files)[1] in keepExts:
-            try:
-                # Move file(s) to destDir
-                shutil.move(data[0] + os.sep + files, destDir)
-                movedFiles.append(files)
+    # Check to see if user wants to skip current dir
+    if data[0] not in skipDirs:
+        for file in data[2]:
+            # Check to see if file ext matches any ext listed in 'keepExts'
+            if os.path.splitext(file)[1] in keepExts:
+                try:
+                    shutil.move(data[0] + os.sep + file, destDir)
+                    movedFiles.append(file)
 
-            except shutil.Error:
-                continue
+                except shutil.Error:
+                    continue
 
-print("Successfully moved the following file(s) into " + cwd + ":\n")
+print("\nSuccessfully moved the following file(s) into " + destDir + ":")
 for i in movedFiles:
     print(i)
 
