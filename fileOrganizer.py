7804067d32a21a622abbf7e9281261c5c778add9
cevans3505 <cevans3505@gmail.com>
2017-09-03 10:18:59 -0400

    v0.4

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -4,73 +4,95 @@
 Created by Distilled
 Contact: distilled@protonmail.com
 3 September 2017
-Version 0.3
+Version 0.4
 """
 
-# Simple Python script to extract all files from
-# subdirectories into the parent directory.
+# Python script to extract all files from sub-directories into a user defined location.
 
-import shutil
-import os 
+############################# BEGIN USER CONFIGURATION #########################################
 
 # What file extensions do you want to preserve? (Wildcard character: *)
 # Example 1 (Extract specific file types):  keepExts = [".png", ".txt", ".mp*"]
 # Example 2 (Extract all file types):       keepExts = [".*"]
-keepExts = []
+keepExts = [".*"]
 
 # Define destination directory (absolute dir path)
 # Default (if blank): parent dir
+# Example (Windows):    destDir = 'C:\\Users\administrator\\Desktop\\Extracted'
+# Example (Unix/Linux): destDir = '/home/distilled/Desktop/Extracted'
 destDir = ''
 
-# What directories do you want to omit from extraction & deletion routines? (absolute dir paths)
+# What directories do you want to omit from extraction & deletion routines? (absolute dir path)
+# Note: The destination directory defined above (destDir) is already included
+# Example (Windows):    skipDirs = ['C:\\Users\administrator\\Desktop\\dir1', 'C:\\Users\administrator\\Desktop\\dir2']
+# Example (Unix/Linux): skipDirs = ['/home/distilled/Desktop/dir1', '/home/distilled/Desktop/dir2']
 skipDirs = []
 
+############################ END USER CONFIGURATION ############################################
+
+
+import shutil
+import os
+
 # Get absolute dir path where script executed
-cwd = os.getcwd()
+rootDir = os.getcwd()
 
 # Create empty arrays to contain log info for output to console at end of script
 movedFiles = []
 deletedDirectories = []
 
-if destDir == "":
-    destDir = cwd
+# Set destination directory to default value (parent dir) unless otherwise defined in user configuation section
+if destDir == '':
+    destDir = rootDir
 else:
     destDir = destDir
 
-# 1st walk
-walker = os.walk(cwd)
+# Add destination directory to list of skipped directories
+skipDirs.append(destDir)
+
+# Get basename of each dir in skipDirs (for use in directory deletion routine)
+skipDirsBasename = []
+for i in skipDirs:
+    skipDirsBasename.append(os.path.basename(i))
+
+# 1st directory walk DOWN the directory tree - File extraction routine
+walker = os.walk(rootDir)
 
 # 'data' returns a 3 index tuple (dirpath, dirnames, filenames)
-for data in walker:
+for roots, dirs, files in walker:
     # Check to see if user wants to skip current dir
-    if data[0] not in (skipDirs or destDir):
-        for file in data[2]:
+    if roots not in skipDirs:
+        for file in files:
             # Check to see if file ext matches any ext listed in 'keepExts'
             if os.path.splitext(file)[1] in keepExts:
                 try:
-                    shutil.move(data[0] + os.sep + file, destDir)
+                    shutil.move(roots + os.sep + file, destDir)
                     movedFiles.append(file)
 
                 except shutil.Error:
                     continue
 
-# 2nd walk
-walker = os.walk(cwd)
+# 2nd directory walk DOWN the directory tree - Directory deletion routine
+walker = os.walk(rootDir)
 
 # 'data' returns a 3 index tuple (dirpath, dirnames, filenames)
-for data in walker:
-    for folders in data[1]:
-        if folders not in ["destDir", "skipDir"]:
-            shutil.rmtree(cwd + os.sep + folders)
-            deletedDirectories.append(folders)
+for roots, dirs, files in walker:
+    for curDir in dirs:
+        if curDir not in skipDirsBasename:
+            try:
+                shutil.rmtree(rootDir + os.sep + curDir)
+                deletedDirectories.append(curDir)
+				
+            except shutil.Error:
+                continue
 
 print("\nSuccessfully moved the following file(s) into " + destDir + ":")
 for i in movedFiles:
     print(i)
 
-print("\nSuccessfully deleted the following directories: ")
+print("\nThe following sub-directories (including all files remaining within) were deleted:")
 for i in deletedDirectories:
     print(i)
 
-input("Press 'Enter' key to exit.")
+#input("Press 'Enter' key to exit.")
 exit()
